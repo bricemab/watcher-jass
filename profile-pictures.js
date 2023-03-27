35e27f1783f1c77e133f06ce120ada326b9fe445
bricemab <55409187+bricemab@users.noreply.github.com>
2023-03-27 12:35:24 +0200

    profile picture

--- profile-pictures.js
+++ profile-pictures.js
@@ -1,5 +1,6 @@
 const config = require("./config");
 const path = require("path");
+const fs = require("fs");
 
 class ProfilePictures {
   static async onNewProfilePicture(p) {
@@ -7,7 +8,13 @@ class ProfilePictures {
       config.livePath,
       "profile-pictures/"
     )
-    console.log(p+ " move to " + destinationPath)
+    let pathSplit = path.split("\\");
+    if (pathSplit.length === 1) {
+      pathSplit = pathSplit[0].split('/');
+    }
+    const filename = pathSplit[pathSplit.length-1]
+    console.log(p+ " move to " + destinationPath);
+    await fs.renameSync(p, destinationPath+filename);
   }
 }
 
